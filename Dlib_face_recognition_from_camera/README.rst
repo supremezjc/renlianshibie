c5375acfd472974c1b84f08a82131309e1c2a710
supremezjc <50534228+supremezjc@users.noreply.github.com>
2019-07-13 21:00:30 +0800

    Add files via upload

---
+++ Dlib_face_recognition_from_camera/README.rst
@@ -0,0 +1,207 @@
+Face recognition from camera with Dlib
+######################################
+
+Introduction
+************
+
+Detect and recognize single/multi-faces from camera;
+
+调用摄像头进行人脸识别，支持多张人脸同时识别;
+
+
+#. 摄像头人脸录入 / Face register
+
+   .. image:: introduction/get_face_from_camera.png
+      :align: center
+
+   请不要离摄像头过近，人脸超出摄像头范围时会有 "OUT OF RANGE" 提醒 /
+   Please do not be too close to the camera, or you can't save faces with "OUT OF RANGE" warning;
+
+   .. image:: introduction/get_face_from_camera_out_of_range.png
+      :align: center
+
+#. 提取特征建立人脸数据库 / Generate database from images captured
+#. 利用摄像头进行人脸识别 / Face recognizer
+   
+   当单张人脸 / When single-face:
+   
+   .. image:: introduction/face_reco_single_person.png
+      :align: center
+
+   当多张人脸 / When multi-faces:
+
+   一张已录入人脸 + 未录入 unknown 人脸 / 1x known face + 1x unknown face:
+
+   .. image:: introduction/face_reco_two_people.png
+      :align: center
+
+   同时识别多张已录入人脸 / multi-faces recognition at the same time:
+
+   .. image:: introduction/face_reco_two_people_in_database.png
+      :align: center
+
+** 关于精度 / About accuracy:
+
+* When using a distance threshold of ``0.6``, the dlib model obtains an accuracy of ``99.38%`` on the standard LFW face recognition benchmark.
+
+** 关于算法 / About algorithm
+
+* 基于 Residual Neural Network / 残差网络的 CNN 模型;
+
+* This model is a ResNet network with 29 conv layers. It's essentially a version of the ResNet-34 network from the paper Deep Residual Learning for Image Recognition by He, Zhang, Ren, and Sun with a few layers removed and the number of filters per layer reduced by half.
+
+If you are interested in the algorithm of face recognition in dlib, please visit http://blog.dlib.net/2017/02/high-quality-face-recognition-with-deep.html for more information.
+
+Overview
+********
+
+此项目中人脸识别的实现流程 / The design of this repo:
+
+.. image:: introduction/overview.png
+   :align: center
+
+Steps
+*****
+
+#. 下载源码 / Download zip from website or via GitHub Desktop in windows, or git clone in Ubuntu
+
+   .. code-block:: bash
+
+      git clone https://github.com/coneypo/Dlib_face_recognition_from_camera
+
+#. 进行 face register / 人脸信息采集录入
+
+   .. code-block:: bash
+
+      python3 get_face_from_camera.py
+
+#. 提取所有录入人脸数据存入 features_all.csv / Features extraction and save into features_all.csv
+
+   .. code-block:: bash
+
+      python3 features_extraction_to_csv.py
+
+#. 调用摄像头进行实时人脸识别 / Real-time face recognition
+
+   .. code-block:: bash
+
+      python3 face_reco_from_camera.py
+
+
+About Source Code
+*****************
+
+Repo 的 tree / 树状图:
+
+::
+
+    .
+    ├── get_faces_from_camera.py        # Step1. Faces register
+    ├── features_extraction_to_csv.py   # Step2. Features extraction
+    ├── face_reco_from_camera.py        # Step3. Faces recognition
+    ├── how_to_use_camera.py            # Use the default camera by opencv
+    ├── data
+    │   ├── data_dlib                   # Dlib's model
+    │   │   ├── dlib_face_recognition_resnet_model_v1.dat
+    │   │   ├── shape_predictor_5_face_landmarks.dat
+    │   │   └── shape_predictor_68_face_landmarks.dat
+    │   ├── data_faces_from_camera      # Face images captured from camera (will generate after step 1)
+    │   │   ├── person_1
+    │   │   │   ├── img_face_1.jpg
+    │   │   │   └── img_face_2.jpg
+    │   │   └── person_2
+    │   │       └── img_face_1.jpg
+    │   │       └── img_face_2.jpg
+    │   └── features_all.csv            # CSV to save all the features of known faces (will generate after step 2)
+    ├── introduction                    # Some files for readme.rst
+    │   ├── Dlib_Face_recognition_by_coneypo.pptx
+    │   ├── face_reco_single_person_customize_name.png
+    │   ├── face_reco_single_person.png
+    │   ├── face_reco_two_people_in_database.png
+    │   ├── face_reco_two_people.png
+    │   ├── get_face_from_camera_out_of_range.png
+    │   ├── get_face_from_camera.png
+    │   └── overview.png
+    ├── README.rst
+    └── requirements.txt                # Some python packages needed
+
+用到的 Dlib 相关模型函数:
+
+#. Dlib 正向人脸检测器 (based on HOG), output: <class 'dlib.dlib.rectangles'>
+
+
+   .. code-block:: python
+
+      detector = dlib.get_frontal_face_detector()
+      faces = detector(img_gray, 0)
+
+	  
+#. Dlib 人脸预测器, output: <class 'dlib.dlib.full_object_detection'>
+
+   .. code-block:: python
+
+      predictor = dlib.shape_predictor("data/data_dlib/shape_predictor_5_face_landmarks.dat")
+      shape = predictor(img_rd, faces[i])
+
+	  
+#. 特征描述子 Face recognition model, the object maps human faces into 128D vectors
+
+
+   .. code-block:: python
+
+      face_rec = dlib.face_recognition_model_v1("data/data_dlib/dlib_face_recognition_resnet_model_v1.dat")
+
+
+Python 源码介绍如下:
+
+#. get_face_from_camera.py: 
+
+   进行 Face register / 人脸信息采集录入
+
+   * 请注意存储人脸图片时，矩形框不要超出摄像头范围，要不然无法保存到本地;
+   * 超出会有 "out of range" 的提醒;
+
+
+#. features_extraction_to_csv.py:
+     
+   从上一步存下来的图像文件中，提取人脸数据存入CSV;
+  
+   * 会生成一个存储所有特征人脸数据的 "features_all.csv"；
+   * size: n*128 , n means n people you registered and 128 means 128D features of the face
+
+
+#. face_reco_from_camera.py: 
+
+   这一步将调用摄像头进行实时人脸识别; / This part will implement real-time face recognition;
+  
+   * Compare the faces captured from camera with the faces you have registered which are saved in "features_all.csv"
+   
+   * 将捕获到的人脸数据和之前存的人脸数据进行对比计算欧式距离, 由此判断是否是同一个人;
+   
+
+More
+****
+
+Tips:
+
+#. 如果希望详细了解 dlib 的用法，请参考 Dlib 官方 Python api 的网站 / You can refer to this link for more information of how to use dlib: http://dlib.net/python/index.html
+
+#. Windows下建议不要把代码放到 ``C:\``, 可能会出现权限读取问题 / In windows, we will not recommend that running this repo in dir ``C:\``
+
+#. 代码最好不要有中文路径 / No chinese characters in your code directory
+
+#. 人脸录入的时候先建文件夹再保存图片, 先 ``N`` 再 ``S`` / Press ``N`` before ``S``
+
+#. 修改显示人脸姓名, 参考这个 patch 修改代码 / If you want to customize the names shown instead of "Person 1", "Person 2"..., please apply this patch: https://github.com/coneypo/Dlib_face_recognition_from_camera/commit/0351cc0f1a1c3a106102c0671dd19edd5866fa93
+
+可以访问我的博客获取本项目的更详细介绍，如有问题可以邮件联系我 /
+For more details, please refer to my blog (in chinese) or mail to me :
+
+* Blog: https://www.cnblogs.com/AdaminXie/p/9010298.html
+  
+* Mail: coneypo@foxmail.com ( Dlib 相关 repo 问题请联系 @foxmail 而不是 @intel )
+
+
+仅限于交流学习, 商业合作勿扰;
+
+Thanks for your support.
---
+++ Dlib_face_recognition_from_camera/get_faces_from_camera.py
@@ -0,0 +1,196 @@
+# 进行人脸录入 / face register
+# 录入多张人脸 / support multi-faces
+
+# Author:   coneypo
+# Blog:     http://www.cnblogs.com/AdaminXie
+# GitHub:   https://github.com/coneypo/Dlib_face_recognition_from_camera
+# Mail:     coneypo@foxmail.com
+
+# Created at 2018-05-11
+# Updated at 2019-04-12
+
+import dlib         # 人脸处理的库 Dlib
+import numpy as np  # 数据处理的库 Numpy
+import cv2          # 图像处理的库 OpenCv
+
+import os           # 读写文件
+import shutil       # 读写文件
+
+# Dlib 正向人脸检测器 / frontal face detector
+detector = dlib.get_frontal_face_detector()
+
+# Dlib 68 点特征预测器 / 68 points features predictor
+predictor = dlib.shape_predictor('data/data_dlib/shape_predictor_68_face_landmarks.dat')
+
+# OpenCv 调用摄像头 use camera
+cap = cv2.VideoCapture(0)
+
+# 设置视频参数 set camera
+cap.set(3, 480)
+
+# 人脸截图的计数器 the counter for screen shoot
+cnt_ss = 0
+
+# 存储人脸的文件夹 the folder to save faces
+current_face_dir = ""
+
+# 保存 faces images 的路径 the directory to save images of faces
+path_photos_from_camera = "data/data_faces_from_camera/"
+
+
+# 新建保存人脸图像文件和数据CSV文件夹
+# mkdir for saving photos and csv
+def pre_work_mkdir():
+
+    # 新建文件夹 / make folders to save faces images and csv
+    if os.path.isdir(path_photos_from_camera):
+        pass
+    else:
+        os.mkdir(path_photos_from_camera)
+
+
+pre_work_mkdir()
+
+
+##### optional/可选, 默认关闭 #####
+# 删除之前存的人脸数据文件夹
+# delete the old data of faces
+def pre_work_del_old_face_folders():
+    # 删除之前存的人脸数据文件夹
+    # 删除 "/data_faces_from_camera/person_x/"...
+    folders_rd = os.listdir(path_photos_from_camera)
+    for i in range(len(folders_rd)):
+        shutil.rmtree(path_photos_from_camera+folders_rd[i])
+
+    if os.path.isfile("data/features_all.csv"):
+        os.remove("data/features_all.csv")
+
+# 这里在每次程序录入之前, 删掉之前存的人脸数据
+# 如果这里打开，每次进行人脸录入的时候都会删掉之前的人脸图像文件夹 person_1/,person_2/,person_3/...
+# If enable this function, it will delete all the old data in dir person_1/,person_2/,/person_3/...
+# pre_work_del_old_face_folders()
+##################################
+
+
+# 如果有之前录入的人脸 / if the old folders exists
+# 在之前 person_x 的序号按照 person_x+1 开始录入 / start from person_x+1
+if os.listdir("data/data_faces_from_camera/"):
+    # 获取已录入的最后一个人脸序号 / get the num of latest person
+    person_list = os.listdir("data/data_faces_from_camera/")
+    person_num_list = []
+    for person in person_list:
+        person_num_list.append(int(person.split('_')[-1]))
+    person_cnt = max(person_num_list)
+
+# 如果第一次存储或者没有之前录入的人脸, 按照 person_1 开始录入
+# start from person_1
+else:
+    person_cnt = 0
+
+# 之后用来控制是否保存图像的 flag / the flag to control if save
+save_flag = 1
+
+# 之后用来检查是否先按 'n' 再按 's' / the flag to check if press 'n' before 's'
+press_n_flag = 0
+
+while cap.isOpened():
+    flag, img_rd = cap.read()
+    # print(img_rd.shape)
+    # It should be 480 height * 640 width
+
+    kk = cv2.waitKey(1)
+
+    img_gray = cv2.cvtColor(img_rd, cv2.COLOR_RGB2GRAY)
+    
+    # 人脸数 faces
+    faces = detector(img_gray, 0)
+
+    # 待会要写的字体 / font to write
+    font = cv2.FONT_HERSHEY_COMPLEX
+
+    # 按下 'n' 新建存储人脸的文件夹 / press 'n' to create the folders for saving faces
+    if kk == ord('n'):
+        person_cnt += 1
+        current_face_dir = path_photos_from_camera + "person_" + str(person_cnt)
+        os.makedirs(current_face_dir)
+        print('\n')
+        print("新建的人脸文件夹 / Create folders: ", current_face_dir)
+
+        cnt_ss = 0              # 将人脸计数器清零 / clear the cnt of faces
+        press_n_flag = 1        # 已经按下 'n' / have pressed 'n'
+
+    # 检测到人脸 / if face detected
+    if len(faces) != 0:
+        # 矩形框 / show the rectangle box
+        for k, d in enumerate(faces):
+            # 计算矩形大小
+            # we need to compute the width and height of the box
+            # (x,y), (宽度width, 高度height)
+            pos_start = tuple([d.left(), d.top()])
+            pos_end = tuple([d.right(), d.bottom()])
+
+            # 计算矩形框大小 / compute the size of rectangle box
+            height = (d.bottom() - d.top())
+            width = (d.right() - d.left())
+
+            hh = int(height/2)
+            ww = int(width/2)
+
+            # 设置颜色 / the color of rectangle of faces detected
+            color_rectangle = (255, 255, 255)
+
+            # 判断人脸矩形框是否超出 480x640
+            if (d.right()+ww) > 640 or (d.bottom()+hh > 480) or (d.left()-ww < 0) or (d.top()-hh < 0):
+                cv2.putText(img_rd, "OUT OF RANGE", (20, 300), font, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+                color_rectangle = (0, 0, 255)
+                save_flag = 0
+                if kk == ord('s'):
+                    print("请调整位置 / Please adjust your position")
+            else:
+                color_rectangle = (255, 255, 255)
+                save_flag = 1
+
+            cv2.rectangle(img_rd,
+                          tuple([d.left() - ww, d.top() - hh]),
+                          tuple([d.right() + ww, d.bottom() + hh]),
+                          color_rectangle, 2)
+
+            # 根据人脸大小生成空的图像 / create blank image according to the size of face detected
+            im_blank = np.zeros((int(height*2), width*2, 3), np.uint8)
+
+            if save_flag:
+                # 按下 's' 保存摄像头中的人脸到本地 / press 's' to save faces into local images
+                if kk == ord('s'):
+                    # 检查有没有先按'n'新建文件夹 / check if you have pressed 'n'
+                    if press_n_flag:
+                        cnt_ss += 1
+                        for ii in range(height*2):
+                            for jj in range(width*2):
+                                im_blank[ii][jj] = img_rd[d.top()-hh + ii][d.left()-ww + jj]
+                        cv2.imwrite(current_face_dir + "/img_face_" + str(cnt_ss) + ".jpg", im_blank)
+                        print("写入本地 / Save into：", str(current_face_dir) + "/img_face_" + str(cnt_ss) + ".jpg")
+                    else:
+                        print("请在按 'S' 之前先按 'N' 来建文件夹 / Please press 'N' before 'S'")
+
+    # 显示人脸数 / show the numbers of faces detected
+    cv2.putText(img_rd, "Faces: " + str(len(faces)), (20, 100), font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+
+    # 添加说明 / add some statements
+    cv2.putText(img_rd, "Face Register", (20, 40), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(img_rd, "N: New face folder", (20, 350), font, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(img_rd, "S: Save current face", (20, 400), font, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(img_rd, "Q: Quit", (20, 450), font, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+
+    # 按下 'q' 键退出 / press 'q' to exit
+    if kk == ord('q'):
+        break
+
+    # 如果需要摄像头窗口大小可调 / uncomment this line if you want the camera window is resizeable
+    # cv2.namedWindow("camera", 0)
+
+    cv2.imshow("camera", img_rd)
+
+# 释放摄像头 / release camera
+cap.release()
+
+cv2.destroyAllWindows()
